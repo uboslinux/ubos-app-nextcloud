83b3182e5343379c45fad3b4546eb5ca688ed0f7
Johannes Ernst <jernst@indiecomputing.com>
2018-11-03 03:22:42 +0000

    Prime-caches does not work yet: installer runs at the time the site isn't back available through the web

--- nextcloud/bin/prime-caches.pl
+++ nextcloud/bin/prime-caches.pl
@@ -14,17 +14,56 @@ use UBOS::Utils;
 if(    'install' eq $operation
     || 'upgrade' eq $operation )
 {
+    my $adminlogin  = $config->getResolve( 'site.admin.userid' );
+    my $adminpass   = $config->getResolve( 'site.admin.credential' );
+
     my $proto    = $config->getResolve( 'site.protocol' );
     my $hostname = $config->getResolve( 'site.hostname' );
     my $context  = $config->getResolve( 'appconfig.context' );
-    my $url      = "$proto$hostname$context";
 
+    my $url;
+    if( '*' eq $hostname ) {
+        $url = "$proto://127.0.0.1$context";
+    } else {
+        $url = "$proto://$hostname$context";
+    }
+
+    my $ok       = 1; # don't continue if something failed, but don't interrupt install; not critical
+
+    my $cookieFile = File::Temp->new();
+    my $loginData;
+
+info( "XXX ABOUT TO GO TO FRONT PAGE" );
     my $out;
-    if( UBOS::Utils::myexec( "curl $url/", undef, \$out, \$out )) {
-        error( 'curl prime-caches (1) failed:', $out );
+    if( $ok && UBOS::Utils::myexec( "curl --insecure --cookie-jar '$cookieFile' -b '$cookieFile' $url/login", undef, \$out, \$out )) {
+        warning( 'curl prime-caches (1) failed for Nextcloud:', $out );
+        $ok = 0;
 
-    } elsif( UBOS::Utils::myexec( "curl -L $url/apps/files/", undef, \$out, \$out )) {
-        error( 'curl prime-caches (2) failed:', $out );
+    } else {
+        if( $out =~ m!<input.*name="requesttoken".*value="([^"]+)"! ) {
+            my $requestToken = $1;
+            $loginData  = "user=$adminlogin";
+            $loginData .= "&password=$adminpass";
+            $loginData .= "&requesttoken=$requestToken";
+            $loginData .= '&timezone=Etc/UTC';
+            $loginData .= '&timezone_offset=0';
+
+info( "XXX request token is $requestToken" );
+info( "XXX loginData is $loginData" );
+
+        } else {
+            warning( 'Nextcloud login page did not contain requesttoken', $out );
+            $ok = 0;
+        }
+    }
+    getc();
+
+    if( $ok ) {
+info( "XXX ABOUT TO LOGIN" );
+        if( UBOS::Utils::myexec( "curl --insecure --cookie-jar '$cookieFile' -b '$cookieFile' -L $url/login -d $loginData", undef, \$out, \$out )) {
+            warning( 'curl prime-caches (2) failed for Nextcloud:', $out );
+            $ok = 0;
+        }
     }
 }
 
