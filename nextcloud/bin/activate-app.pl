d2e6dbfe59a68890afc391fb6e45a98f2dc82d38
Johannes Ernst <jernst@indiecomputing.com>
2020-07-29 18:33:37 +0000

    Track upstream Added forms app Tone down error when app has incompatible/untested version

--- nextcloud/bin/activate-app.pl
+++ nextcloud/bin/activate-app.pl
@@ -22,7 +22,11 @@ my $out;
 if( 'install' eq $operation || 'upgrade' eq $operation ) {
 
     if( UBOS::Utils::myexec( "$cmdPrefix app:enable $appName", undef, \$out, \$out ) != 0 ) {
-        error( "Activating Nextcloud app $appName failed:", $out );
+        if( $out =~ m!not compatible with this version! ) {
+            info( "Not automatically activating Nextcloud app $appName because it has not been tested with this version of Nextcloud" );
+        } else {
+            error( "Activating Nextcloud app $appName failed:", $out );
+        }
     }
 
 } else {
